a79ff1d3a98a87126aec15b593ea66308b243aae
prolic <saschaprolic@googlemail.com>
2015-08-06 22:04:29 +0100

    initial mongo db adapter
    based on: https://github.com/prooph/event-store-mongodb-adapter/pull/4

--- src/EventStore/EventStoreFactory.php
+++ src/EventStore/EventStoreFactory.php
@@ -65,12 +65,20 @@ final class EventStoreFactory implements FactoryInterface
         //Check if we have to use the application wide database connection
         if ($adapterType == 'Prooph\EventStore\Adapter\Doctrine\DoctrineEventStoreAdapter'
             && !isset($adapterOptions['connection'])
-            && isset($adapterOptions['doctrine_connection_alias'])) {
+            && isset($adapterOptions['doctrine_connection_alias'])
+        ) {
             $config['adapter']['options']['connection'] = $serviceLocator->get('doctrine.connection.' . $adapterOptions['doctrine_connection_alias']);
         } else if ( $adapterType == 'Prooph\EventStore\Adapter\Zf2\Zf2EventStoreAdapter'
             && isset($adapterOptions['zend_db_adapter'])
-            && is_string($adapterOptions['zend_db_adapter'])) {
+            && is_string($adapterOptions['zend_db_adapter'])
+        ) {
             $config['adapter']['options']['zend_db_adapter'] = $serviceLocator->get($adapterOptions['zend_db_adapter']);
+        } else if ($adapterType == 'Prooph\EventStore\Adapter\MongoDb\MongoDbEventStoreAdapter'
+            && !isset($adapterOptions['mongo_client'])
+        ) {
+            isset($adapterOptions['mongo_connection_alias'])
+                ? $config['adapter']['options']['mongo_client'] = $serviceLocator->get($adapterOptions['mongo_connection_alias'])
+                : $config['adapter']['options']['mongo_client'] = new \MongoClient();
         }
 
         $featureManagerConfig = null;
