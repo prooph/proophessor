34a374770c64e9f830308b67da144582bc076afe
Alexander Miertsch <kontakt@codeliner.ws>
2017-04-14 11:57:42 +0200

    Change support mail, update fun facts

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
 function loadFunFacts(funFactsResource) {
+    //Animated Number
+    $.fn.animateNumbers = function(stop, commas, duration, ease) {
+        return this.each(function() {
+            var $this = $(this);
+            var start = parseInt($this.text().replace(/,/g, ""));
+            commas = (commas === undefined) ? true : commas;
+            $({value: start}).animate({value: stop}, {
+                duration: duration == undefined ? 1000 : duration,
+                easing: ease == undefined ? "swing" : ease,
+                step: function() {
+                    $this.text(Math.floor(this.value));
+                    if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+                },
+                complete: function() {
+                    if (parseInt($this.text()) !== stop) {
+                        $this.text(stop);
+                        if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+                    }
+                }
+            });
+        });
+    };
+
     $.get(funFactsResource).then(function (response) {
         $('#animated-number').removeClass('hidden');
 
         $('#animated-number-downloads').data('digit', response.fun_facts.downloads);
         $('#animated-number-stars').data('digit', response.fun_facts.github_stars);
         $('#animated-number-people').data('digit', response.fun_facts.gitter_people.length);
+
+        $('.animated-number').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+            var $this = $(this);
+            if (visible) {
+                $this.animateNumbers($this.data('digit'), false, $this.data('duration'));
+                $this.unbind('inview');
+            }
+        });
 
         $('#community').removeClass('hidden');
 
@@ -198,37 +229,6 @@ jQuery(function($) {'use strict';
 			if (visible) {
 				$(this).css('width', $(this).data('width') + '%');
 				$(this).unbind('inview');
-			}
-		});
-
-		//Animated Number
-		$.fn.animateNumbers = function(stop, commas, duration, ease) {
-			return this.each(function() {
-				var $this = $(this);
-				var start = parseInt($this.text().replace(/,/g, ""));
-				commas = (commas === undefined) ? true : commas;
-				$({value: start}).animate({value: stop}, {
-					duration: duration == undefined ? 1000 : duration,
-					easing: ease == undefined ? "swing" : ease,
-					step: function() {
-						$this.text(Math.floor(this.value));
-						if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
-					},
-					complete: function() {
-						if (parseInt($this.text()) !== stop) {
-							$this.text(stop);
-							if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
-						}
-					}
-				});
-			});
-		};
-
-		$('.animated-number').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-			var $this = $(this);
-			if (visible) {
-				$this.animateNumbers($this.data('digit'), false, $this.data('duration')); 
-				$this.unbind('inview');
 			}
 		});
 	});
