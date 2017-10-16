0cd5d4080fe9b404c23f758b47d95d2bcc6dd1ac
codeliner <kontakt@codeliner.ws>
2017-10-16 07:59:18 +0200

    Assign default repo upfront

--- template/helper/forkOnGithub.php
+++ template/helper/forkOnGithub.php
@@ -2,15 +2,12 @@
 
 final class forkOnGithub
 {
-    private $githubRepo;
+    private $githubRepo = 'https://github.com/prooph/proophessor';
 
     public function __construct(\Bookdown\Bookdown\Config\IndexConfig $config)
     {
-        $default = 'https://github.com/prooph/proophessor';
         if($config->isRemote()) {
-            $this->githubRepo = $this->extractGithubRepo($config, $default);
-        } else {
-            $this->githubRepo = $default;
+            $this->githubRepo = $this->extractGithubRepo($config);
         }
     }
 
@@ -19,13 +16,11 @@ final class forkOnGithub
         return '<span id="forkongithub"><a href="'.$this->githubRepo.'">Fork me on GitHub</a></span>';
     }
 
-    private function extractGithubRepo(\Bookdown\Bookdown\Config\IndexConfig $config, $default = null)
+    private function extractGithubRepo(\Bookdown\Bookdown\Config\IndexConfig $config)
     {
         $match = [];
         if(preg_match('/^https:\/\/raw.githubusercontent.com\/(?P<orga>[\w-_]+)\/(?P<repo>[\w-_]+)\/.*$/', $config->getFile(), $match)) {
             return 'https://github.com/' . $match['orga'] . '/' . $match['repo'];
-        } else {
-            return $default;
-        };
+        }
     }
 }
